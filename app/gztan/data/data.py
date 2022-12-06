9020239b749b94266906e080ce39dec39e901f2c
michalgorul <michalgorul@gmail.com>
2022-12-06 21:42:49 +0100

    add 3 sec data generator

--- app/gztan/data/data.py
+++ app/gztan/data/data.py
@@ -2,11 +2,14 @@ from keras.preprocessing.image import ImageDataGenerator, DirectoryIterator
 
 from app.gztan.data.gztan import gtzan
 
-train_dir = gtzan.directories_10sec.get("train_dir")
-val_dir = gtzan.directories_10sec.get("val_dir")
+train_10sec_dir = gtzan.directories_10sec.get("train_dir")
+val_10sec_dir = gtzan.directories_10sec.get("val_dir")
+
+train_3sec_dir = gtzan.directories_3sec.get("train_dir")
+val_3sec_dir = gtzan.directories_3sec.get("val_dir")
 
 
-def get_train_data_generator() -> DirectoryIterator:
+def get_train_data_generator(sec_3: bool = False) -> DirectoryIterator:
     """
     Yields batches of 150 ×150 RGB train images (shape (20, 150, 150, 3)) and categorical labels
     (shape(20,10)). There are 20 samples in each batch (the batch size). It yields these batches indefinitely:
@@ -17,6 +20,8 @@ def get_train_data_generator() -> DirectoryIterator:
     print("Creating train data generator")
     train_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
+    train_dir = train_3sec_dir if sec_3 else train_10sec_dir
+
     train_generator = train_datagen.flow_from_directory(
         train_dir, target_size=(150, 150), batch_size=20, class_mode="categorical"
     )
@@ -24,7 +29,7 @@ def get_train_data_generator() -> DirectoryIterator:
     return train_generator
 
 
-def get_validation_data_generator() -> DirectoryIterator:
+def get_validation_data_generator(sec_3: bool = False) -> DirectoryIterator:
     """
     Yields batches of 150 ×150 RGB validation images (shape (20, 150, 150, 3)) and categorical labels
     (shape(20,10)). There are 20 samples in each batch (the batch size). It yields these batches indefinitely:
@@ -35,8 +40,10 @@ def get_validation_data_generator() -> DirectoryIterator:
     print("Creating validation data generator")
     validation_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
+    validation_dir = val_3sec_dir if sec_3 else val_10sec_dir
+
     validation_generator = validation_datagen.flow_from_directory(
-        val_dir, target_size=(150, 150), batch_size=20, class_mode="categorical"
+        validation_dir, target_size=(150, 150), batch_size=20, class_mode="categorical"
     )
     print()
     return validation_generator
